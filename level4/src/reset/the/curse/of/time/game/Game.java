99534328b3440dd25c2f3fff1893feeff8f90bb3
luife-correia <lfc291006@gmail.com>
2026-02-25 11:55:23 -0300

    Adição da introdução

---
+++ level4/src/reset/the/curse/of/time/game/Game.java
@@ -0,0 +1,136 @@
+package reset.the.curse.of.time.game;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Game {
+
+    static String nome;
+    static int vida = 100;
+    static int xp = 0;
+    static int respostasCertas = 0;
+    static int respostasErradas = 0;
+    static int acertosBoss = 0;
+
+    static Scanner input = new Scanner(System.in);
+    static Random random = new Random();
+
+    //metodo para efeito de digitação.
+    static void digitando(String texto) {
+        for (int i = 0; i < texto.length(); i++) {
+            System.out.print(texto.charAt(i));
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println();
+    }
+
+    //metodo para mudar cor do texto quando for lore.
+    static void textoLore(String lore){
+        digitando("\u001B[38;5;24m" + lore + "\u001B[0m");
+    }
+
+    //metodo para mudar cor do texto quando for aviso.
+    static void aviso(String alerta){
+        digitando("\u001B[38;5;196m" + alerta + "\u001B[0m");
+    }
+
+    public void jogar() {
+
+        //texto introdutório
+        textoLore("\nHá muito tempo, em um mundo onde o tempo não é uma linha, mas um círculo imperfeito,");
+        textoLore("existe uma lenda sobre um artefato capaz de dobrar a realidade: o Laço Infinito.");
+        textoLore("Dizem que quem o possuir poderá reescrever eventos, reviver momentos ou até escapar do próprio destino.");
+        textoLore("Mas o Laço não está em qualquer lugar — ele repousa no coração de um labirinto temporal,");
+        textoLore("uma prisão de ciclos eternos onde o tempo se reinicia sempre que alguém falha em sua jornada.\n");
+
+        //definir nome do player
+        do {
+            digitando("Digite o nome do seu herói ou heroína: ");
+            nome = input.nextLine();
+        }while(nome.isEmpty());
+        textoLore("Você é "+nome+", alguém em busca do Laço Infinito, mas o motivo ainda é oculto.\n");
+
+        // Escolha de tempo
+        String escolhaTempo = "";
+        while (!escolhaTempo.equals("passado") && !escolhaTempo.equals("futuro")) {
+            digitando(nome+" para qual parte do tempo você deseja ir com o Laço Infinito (Passado ou Futuro)? ");
+            escolhaTempo = input.nextLine().trim().toLowerCase();
+        }
+
+        String motivo = "";
+        int opcao;
+        // Oferece opções conforme a escolha
+        if (escolhaTempo.equals("passado")){
+            do {
+                digitando("O que você deseja mudar?");
+                digitando("1 - Impedir a morte de alguém querido");
+                digitando("2 - Evitar uma guerra que destruiu milhões de vidas");
+                digitando("3 - Reviver um momento que nunca deveria ter acabado");
+                digitando("4 - Salvar uma civilização perdida");
+                digitando("5 - Recuperar uma oportunidade que você deixou escapar");
+                System.out.print("Escolha uma opção (1/2/3/4/5):");
+                opcao = input.nextInt();
+                input.nextLine();} // Limpa o buffer
+            while(opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5 );
+
+            switch (opcao) {
+                case 1 -> motivo = "Impedir a morte de alguém querido";
+                case 2 -> motivo = "Evitar uma guerra que destruiu milhões de vidas";
+                case 3 -> motivo = "Reviver um momento que nunca deveria ter acabado";
+                case 4 -> motivo = "Salvar uma civilização perdida";
+                case 5 -> motivo = "Recuperar uma oportunidade que você deixou escapar";
+            }
+
+        } else {
+            do {
+                digitando("O que você deseja descobrir?");
+                digitando("1 - Explorar como será a vida em outros planetas");
+                digitando("2 - Saber se a humanidade encontrou cura para todas as doenças");
+                digitando("3 - Descobrir as novas invenções futuras do seu mundo");
+                digitando("4 - Conhecer a sociedade do futuro e suas regras");
+                digitando("5 - Obter informações para mudar sua vida atual");
+                System.out.print("Escolha uma opção (1/2/3/4/5):");
+                opcao = input.nextInt();
+                input.nextLine();} // Limpa o buffer
+            while (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5 );
+            switch (opcao) {
+                case 1 -> motivo = "Explorar como será a vida em outros planetas";
+                case 2 -> motivo = "Saber se a humanidade encontrou cura para todas as doenças";
+                case 3 -> motivo = "Descobrir as novas invenções futuras do seu mundo";
+                case 4 -> motivo = "Conhecer a sociedade do futuro e suas regras";
+                case 5 -> motivo = "Obter informações para mudar sua vida atual";
+            }
+        }
+
+        // Narrativa final antes do inicio da run
+        textoLore("\nCom o propósito de "+motivo.toLowerCase() +".");
+        textoLore("Você parte em direção ao Labirinto de Chronos, onde o Laço Infinito repousa.");
+        textoLore("\nApós semanas de viagem por terras esquecidas,");
+        textoLore("enfrentando criaturas que parecem existir fora do tempo,");
+        textoLore("corvos que falam em enigmas, árvores que crescem ao contrário,");
+        textoLore("e rios que correm para o céu. Você finalmente chega à entrada do labirinto.");
+        textoLore("É uma estrutura viva, feita de pedra pulsante e relógios quebrados.");
+        textoLore("O ar vibra com ecos de vozes que ainda não foram ditas.\n");
+        textoLore("Um aviso está gravado na entrada:");aviso("Aqui, o tempo não cura. Ele repete.”\n");
+        textoLore("Você respira fundo. A primeira tentativa está prestes a começar.\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
---
+++ level4/src/reset/the/curse/of/time/main/Main.java
@@ -0,0 +1,7 @@
+package reset.the.curse.of.time.main;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
